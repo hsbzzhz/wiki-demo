207fbd605693dfaa153cd961e4e075ee62878b1e
Hogan Zhang <hsbzzhz@126.com>
2021-09-25 22:22:09 +0800

    test, datasource and mybatis config

--- src/main/java/com/grundfos/wiki/WikiApplication.java
+++ src/main/java/com/grundfos/wiki/WikiApplication.java
@@ -1,13 +1,25 @@
 package com.grundfos.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+//import java.util.logging.Logger;
 
 @SpringBootApplication
 public class WikiApplication {
 
+	//	自定义化日志
+	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
+//		原始启动项
+//		SpringApplication.run(WikiApplication.class, args);
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
