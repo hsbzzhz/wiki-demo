c77a2416d9d676c0b01943caf7f88c35c4bb9ead
Hogan Zhang <hsbzzhz@126.com>
2021-10-03 20:27:42 +0800

    conf file

--- src/main/java/com/grundfos/wiki/config/CorsConfig.java
+++ src/main/java/com/grundfos/wiki/config/CorsConfig.java
@@ -1,9 +1,11 @@
 package com.grundfos.wiki.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
