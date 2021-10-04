ec09aa835ffa727e02799fbb2e1dad4135df73ca
Hogan Zhang <hsbzzhz@126.com>
2021-10-04 09:32:06 +0800

    log: filter; interceptor; aop*

---
+++ src/main/java/com/grundfos/wiki/filter/LogFilter.java
@@ -0,0 +1,38 @@
+package com.grundfos.wiki.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * 过滤器
+ * 使用到 filterChain
+ */
+
+//@Component
+//public class LogFilter implements Filter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        // 打印请求信息
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("------------- LogFilter 开始 -------------");
+//        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+//        LOG.info("远程地址: {}", request.getRemoteAddr());
+//
+//        long startTime = System.currentTimeMillis();
+//        filterChain.doFilter(servletRequest, servletResponse); // 内部处理业务代码，运行程序
+//        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//    }
+//}
