75bd919333ef930f4eb00156b6e70537b9a62cc9
Hogan Zhang <hsbzzhz@126.com>
2021-10-08 10:57:31 +0800

    Category part backend

---
+++ src/main/java/com/grundfos/wiki/req/CategoryQueryReq.java
@@ -0,0 +1,7 @@
+package com.grundfos.wiki.req;
+
+public class CategoryQueryReq extends PageReq{
+    public String toString() {
+        return "CategoryQueryReq{} " + super.toString();
+    }
+}
---
+++ src/main/java/com/grundfos/wiki/service/CategoryService.java
@@ -0,0 +1,70 @@
+package com.grundfos.wiki.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.grundfos.wiki.entity.Category;
+import com.grundfos.wiki.entity.CategoryExample;
+import com.grundfos.wiki.mapper.CategoryMapper;
+import com.grundfos.wiki.req.CategoryQueryReq;
+import com.grundfos.wiki.req.CategorySaveReq;
+import com.grundfos.wiki.resp.CategoryQueryResp;
+import com.grundfos.wiki.resp.PageResp;
+import com.grundfos.wiki.util.CopyUtil;
+import com.grundfos.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryService.class);
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<CategoryQueryResp> list(CategoryQueryReq req) {
+        CategoryExample categoryExample = new CategoryExample();
+        CategoryExample.Criteria criteria = categoryExample.createCriteria();
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+
+        PageInfo<Category> pageInfo = new PageInfo<>(categoryList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        // 列表复制
+        List<CategoryQueryResp> list = CopyUtil.copyList(categoryList, CategoryQueryResp.class);
+
+        PageResp<CategoryQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+        return pageResp;
+    }
+
+    /**
+     * 保存
+     */
+    public void save(CategorySaveReq req) {
+        Category category = CopyUtil.copy(req, Category.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            category.setId(snowFlake.nextId());
+            categoryMapper.insert(category);
+        } else {
+            // 更新
+            categoryMapper.updateByPrimaryKey(category);
+        }
+    }
+
+    public void delete(Long id) {
+        categoryMapper.deleteByPrimaryKey(id);
+    }
+}
