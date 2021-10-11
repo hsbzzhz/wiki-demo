a817d63cd67599c210d673d47dbf13b385a01d9e
Hogan Zhang <hsbzzhz@126.com>
2021-10-11 10:06:39 +0800

    添加富文本插件，wangEditor@4.6.3，建表，垂直分表

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -79,6 +79,9 @@
             <a-form-item label="顺序">
                 <a-input v-model:value="doc.sort" />
             </a-form-item>
+            <a-form-item label="内容">
+                <div id="content"></div>
+            </a-form-item>
         </a-form>
     </a-modal>
 </template>
@@ -90,6 +93,7 @@
   import {Tool} from "@/util/tool";
   import {useRoute} from "vue-router";
   import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
+  import E from 'wangeditor'
 
   export default defineComponent({
     name: 'AdminDoc',
@@ -171,6 +175,7 @@
       const doc = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
+      const editor = new E('#content');
       const handleModalOk = () => {
         modalLoading.value = true;
         axios.post("/doc/save", doc.value).then((response) => {
@@ -267,6 +272,9 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
+        setTimeout(function () {
+          editor.create();
+        }, 100);
       };
 
       /**
@@ -280,7 +288,9 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
-
+        setTimeout(function () {
+          editor.create();
+        }, 100);
       };
       /**
        * 删除
