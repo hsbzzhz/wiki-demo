0bfbd838ccd62760c30443bffa5e4526eb9c871d
Hogan Zhang <hsbzzhz@126.com>
2021-10-17 16:31:12 +0800

    user table

---
+++ src/main/java/com/grundfos/wiki/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.grundfos.wiki.controller;
+
+import com.grundfos.wiki.req.UserQueryReq;
+import com.grundfos.wiki.req.UserSaveReq;
+import com.grundfos.wiki.resp.CommonResp;
+import com.grundfos.wiki.resp.UserQueryResp;
+import com.grundfos.wiki.resp.PageResp;
+import com.grundfos.wiki.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid UserQueryReq req) {
+        CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
+        PageResp<UserQueryResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        userService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        userService.delete(id);
+        return resp;
+    }
+}
---
+++ src/main/java/com/grundfos/wiki/service/UserService.java
@@ -0,0 +1,71 @@
+package com.grundfos.wiki.service;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.grundfos.wiki.entity.User;
+import com.grundfos.wiki.entity.UserExample;
+import com.grundfos.wiki.mapper.UserMapper;
+import com.grundfos.wiki.req.UserQueryReq;
+import com.grundfos.wiki.req.UserSaveReq;
+import com.grundfos.wiki.resp.UserQueryResp;
+import com.grundfos.wiki.resp.PageResp;
+import com.grundfos.wiki.util.CopyUtil;
+import com.grundfos.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserService {
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+    @Resource
+    private UserMapper userMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<UserQueryResp> list(UserQueryReq req) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        if (!ObjectUtils.isEmpty(req.getLoginName())) {
+            criteria.andLoginNameEqualTo(req.getLoginName());
+        }
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<User> userList = userMapper.selectByExample(userExample);
+
+        PageInfo<User> pageInfo = new PageInfo<>(userList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        // 列表复制
+        List<UserQueryResp> list = CopyUtil.copyList(userList, UserQueryResp.class);
+
+        PageResp<UserQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+        return pageResp;
+    }
+
+    /**
+     * 保存
+     */
+    public void save(UserSaveReq req) {
+        User user = CopyUtil.copy(req, User.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            user.setId(snowFlake.nextId());
+            userMapper.insert(user);
+        } else {
+            // 更新
+            userMapper.updateByPrimaryKey(user);
+        }
+    }
+
+    public void delete(Long id) {
+        userMapper.deleteByPrimaryKey(id);
+    }
+}
