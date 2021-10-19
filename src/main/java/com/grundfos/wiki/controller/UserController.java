f0a63df42fe14707c860ad9935f638aa128c62e0
Hogan Zhang <hsbzzhz@126.com>
2021-10-19 15:50:27 +0800

    login 写入 cache 返回 key

--- src/main/java/com/grundfos/wiki/controller/UserController.java
+++ src/main/java/com/grundfos/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.grundfos.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.grundfos.wiki.req.UserLoginReq;
 import com.grundfos.wiki.req.UserQueryReq;
 import com.grundfos.wiki.req.UserResetPasswordReq;
@@ -9,17 +10,31 @@ import com.grundfos.wiki.resp.UserLoginResp;
 import com.grundfos.wiki.resp.UserQueryResp;
 import com.grundfos.wiki.resp.PageResp;
 import com.grundfos.wiki.service.UserService;
+import com.grundfos.wiki.util.SnowFlake;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -57,6 +72,12 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
