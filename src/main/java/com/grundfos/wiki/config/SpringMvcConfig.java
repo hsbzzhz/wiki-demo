2bc7ed9ba3f17d6b7f110762080f1ae3fdcbe848
Hogan Zhang <hsbzzhz@126.com>
2021-10-20 17:12:06 +0800

    接口实现拦截器，验证token

--- src/main/java/com/grundfos/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/grundfos/wiki/config/SpringMvcConfig.java
@@ -7,16 +7,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//    /*
-//     * 创建登录拦截器
-//     */
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    /*
+     * 创建登录拦截器
+     */
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**");
+    }
+}
