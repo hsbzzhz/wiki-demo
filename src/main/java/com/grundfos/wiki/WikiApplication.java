22950f3630550041c1f6c9086db6d6cd2adfd5fe
Hogan Zhang <hsbzzhz@126.com>
2021-10-24 22:58:02 +0800

    增加定时任务，和自定义sql 实现后台更新点赞数和阅览数到总览页

--- src/main/java/com/grundfos/wiki/WikiApplication.java
+++ src/main/java/com/grundfos/wiki/WikiApplication.java
@@ -7,11 +7,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //import java.util.logging.Logger;
 
 @SpringBootApplication
 //@MapperScan("{com.grundfos.wiki.mapper}")
+@EnableScheduling
 public class WikiApplication {
 
 	//	自定义化日志
