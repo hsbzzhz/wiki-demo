179025d368434a519f09e5b41a8353b0dc2977f5
Hogan Zhang <hsbzzhz@126.com>
2021-10-25 21:58:59 +0800

    把发送websocket 消息放入mq

--- src/main/java/com/grundfos/wiki/rokectmq/VoteTopicConsumer.java
+++ src/main/java/com/grundfos/wiki/rokectmq/VoteTopicConsumer.java
@@ -1,17 +1,24 @@
 package com.grundfos.wiki.rokectmq;
 
+import com.grundfos.wiki.websocket.WebSocketServer;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Resource;
+
 public class VoteTopicConsumer implements RocketMQListener<MessageExt> {
 
     private static final Logger LOG = LoggerFactory.getLogger(VoteTopicConsumer.class);
+
+    @Resource
+    public WebSocketServer webSocketServer;
 
     @Override
     public void onMessage(MessageExt messageExt) {
         byte[] body = messageExt.getBody();
         LOG.info("ROCKETMQ收到消息：{}", new String(body));
+        webSocketServer.sendInfo(new String(body));
     }
 }
